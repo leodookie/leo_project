518cf7e3195ec2883bd341c727d9f8f3381f393a
Leonardo Win <leo.united@gmail.com>
2019-07-19 01:14:01 -0300

    novo style

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
                   </h3>
                   <div>Testando quebra de linha<br />Testando</div>
                   <div class="container">
-                  <div className="teste_leo">    
+                  <div class="teste_leo">    
                         <Feed> Box 1 </Feed> 
                   </div>
                   </div>
